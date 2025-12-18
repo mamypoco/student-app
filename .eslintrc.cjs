5ced1134629b0fe96172e0052b02f4356a6de9e2
mami <mamypoco@gmail.com>
2025-12-17 19:25:40 -0800

    committed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,6 @@ module.exports = {
       { allowConstantExport: true },
     ],
     semi: ['warn', 'always'],
+    "react/no-unescaped-entities": 0,
   },
 }
---
+++ src/components/StudentList.jsx
@@ -0,0 +1,20 @@
+import './StudentList.css';
+
+const StudentList = () => {
+	const headingClass = 'student-list__heading';
+  const listClass = 'student-list';
+
+  return (
+		<section>
+    <h2 className={headingClass}>Student List</h2>
+      <ul className={listClass}>
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+		</section>
+  )
+}
+
+
+export default StudentList;
